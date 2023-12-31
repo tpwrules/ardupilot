eee712fd9ecd320819fc9bb27d4fb41a5c089aa5
Thomas Watson <twatson52@icloud.com>
2023-12-31 11:41:36 -0600

    AP_ExternalAHRS: start using SBG systems ID and define

--- Tools/scripts/build_options.py
+++ Tools/scripts/build_options.py
@@ -34,6 +34,7 @@ BUILD_OPTIONS = [
     Feature('AHRS', 'AHRS_EXT_MICROSTRAIN5', 'AP_EXTERNAL_AHRS_MICROSTRAIN5_ENABLED', 'Enable MICROSTRAIN 5-series External AHRS', 0, "AHRS_EXT"),  # noqa: E501
     Feature('AHRS', 'AHRS_EXT_MICROSTRAIN7', 'AP_EXTERNAL_AHRS_MICROSTRAIN7_ENABLED', 'Enable MICROSTRAIN 7-series External AHRS', 0, "AHRS_EXT"),  # noqa: E501
     Feature('AHRS', 'AHRS_EXT_VECTORNAV', 'AP_EXTERNAL_AHRS_VECTORNAV_ENABLED', 'Enable VectorNav External AHRS', 0, "AHRS_EXT"),  # noqa
+    Feature('AHRS', 'AHRS_EXT_SBG', 'AP_EXTERNAL_AHRS_SBG_ENABLED', 'Enable SBG Systems External AHRS', 0, "AHRS_EXT"),  # noqa
     Feature('AHRS', 'TEMPCAL', 'HAL_INS_TEMPERATURE_CAL_ENABLE', 'Enable IMU Temperature Calibration', 0, None),
     Feature('AHRS', 'VISUALODOM', 'HAL_VISUALODOM_ENABLED', 'Enable Visual Odometry', 0, 'EKF3_EXTNAV'),
     Feature('AHRS', 'EKF3_EXTNAV', 'EK3_FEATURE_EXTERNAL_NAV', 'Enable External Navigation for EKF3', 0, 'EKF3'),
--- libraries/AP_ExternalAHRS/AP_ExternalAHRS.cpp
+++ libraries/AP_ExternalAHRS/AP_ExternalAHRS.cpp
@@ -56,7 +56,7 @@ const AP_Param::GroupInfo AP_ExternalAHRS::var_info[] = {
     // @Param: _TYPE
     // @DisplayName: AHRS type
     // @Description: Type of AHRS device
-    // @Values: 0:None,1:VectorNav,2:MicroStrain5,7:MicroStrain7
+    // @Values: 0:None,1:VectorNav,2:MicroStrain5,7:MicroStrain7,8:SBG
     // @User: Standard
     AP_GROUPINFO_FLAGS("_TYPE", 1, AP_ExternalAHRS, devtype, HAL_EXTERNAL_AHRS_DEFAULT, AP_PARAM_FLAG_ENABLE),
 
@@ -119,6 +119,12 @@ void AP_ExternalAHRS::init(void)
     case DevType::InertialLabs:
         backend = new AP_ExternalAHRS_InertialLabs(this, state);
         return;
+#endif
+
+#if AP_EXTERNAL_AHRS_SBG_ENABLED
+    // case DevType::SBG:
+    //     backend = new AP_ExternalAHRS_SBG(this, state);
+    //     return;
 #endif
 
     }
