7056a2b16ef942bfd7d7be36dff185ab4733bbad
Andy Piper <github@andypiper.com>
2025-03-06 15:35:33 +0000

    Plane: refactor fence checking into an io callback

--- ArduPlane/Plane.h
+++ ArduPlane/Plane.h
@@ -1070,8 +1070,10 @@ private:
 #if AP_FENCE_ENABLED
     // fence.cpp
     void fence_check();
+    void fence_run_checks() override;
     bool fence_stickmixing() const;
     bool in_fence_recovery() const;
+    uint8_t orig_breaches;
 #endif
 
     // ArduPlane.cpp
