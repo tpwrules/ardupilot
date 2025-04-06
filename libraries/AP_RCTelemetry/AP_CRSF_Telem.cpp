b09d20a58e6cb6a03beafcc49b83b9545867e55e
Thomas Watson <twatson52@icloud.com>
2025-04-05 19:42:38 -0500

    fix compilation on cube orange

--- libraries/AP_RCTelemetry/AP_CRSF_Telem.cpp
+++ libraries/AP_RCTelemetry/AP_CRSF_Telem.cpp
@@ -1212,8 +1212,8 @@ void AP_CRSF_Telem::calc_parameter() {
         AP_Int8* p = (AP_Int8*)param->_param;
         _telem.ext.param_entry.payload[1] = ParameterType::INT8;
         _telem.ext.param_entry.payload[idx] = p->get();  // value
-        _telem.ext.param_entry.payload[idx+1] = int8_t(param->_param_min);  // min
-        _telem.ext.param_entry.payload[idx+2] = int8_t(param->_param_max); // max
+        _telem.ext.param_entry.payload[idx+1] = int8_t(param->_param_min.get());  // min
+        _telem.ext.param_entry.payload[idx+2] = int8_t(param->_param_max.get()); // max
         _telem.ext.param_entry.payload[idx+3] = int8_t(0);  // default
         idx += 4;
         break;
@@ -1222,8 +1222,8 @@ void AP_CRSF_Telem::calc_parameter() {
         AP_Int16* p = (AP_Int16*)param->_param;
         _telem.ext.param_entry.payload[1] = ParameterType::INT16;
         put_be16_ptr(&_telem.ext.param_entry.payload[idx], p->get());  // value
-        put_be16_ptr(&_telem.ext.param_entry.payload[idx+2], param->_param_min);  // min
-        put_be16_ptr(&_telem.ext.param_entry.payload[idx+4], param->_param_max); // max
+        put_be16_ptr(&_telem.ext.param_entry.payload[idx+2], param->_param_min.get());  // min
+        put_be16_ptr(&_telem.ext.param_entry.payload[idx+4], param->_param_max.get()); // max
         put_be16_ptr(&_telem.ext.param_entry.payload[idx+6], 0);  // default
         idx += 8;
         break;
