891a864549f62f0b49d3621e334fb1ab772dfa5b
Thomas Watson <twatson52@icloud.com>
2025-05-14 22:26:13 -0500

    add evil flash access bindings

---
+++ libraries/AP_Scripting/evil_flash_access.cpp
@@ -0,0 +1,85 @@
+#include "AP_Scripting_config.h"
+
+#if AP_SCRIPTING_ENABLED
+
+#include <AP_Common/AP_Common.h>
+#include <AP_Filesystem/AP_Filesystem.h>
+
+#include "evil_flash_access.h"
+
+#include "lua/src/lauxlib.h"
+
+#include <AP_Scripting/AP_Scripting.h>
+#include <AP_Scripting/lua_generated_bindings.h>
+
+#include <AP_HAL_ChibiOS/hwdef/common/flash.h>
+
+int efa_read32(lua_State *L) {
+    binding_argcheck(L, 1);
+
+    const uint32_t addr = (uint32_t)luaL_checkinteger(L, 1);
+
+    __DSB();
+    __ISB();
+
+    const uint32_t val = *((volatile uint32_t*)addr);
+
+    lua_pushinteger(L, (uint32_t)val);
+
+    return 1;
+}
+
+int efa_write32(lua_State *L) {
+    binding_argcheck(L, 2);
+
+    const uint32_t addr = (uint32_t)luaL_checkinteger(L, 1);
+    const uint32_t val = (uint32_t)luaL_checkinteger(L, 2);
+
+    *((volatile uint32_t*)addr) = val;
+
+    __DSB();
+    __ISB();
+
+    return 0;
+}
+
+int efa_erase(lua_State *L) {
+    binding_argcheck(L, 1);
+
+    const uint8_t page = (uint8_t)luaL_checkinteger(L, 1);
+
+    const bool success = stm32_flash_erasepage(page);
+
+    lua_pushboolean(L, success);
+
+    return 1;
+}
+
+int efa_erased(lua_State *L) {
+    binding_argcheck(L, 1);
+
+    const uint8_t page = (uint8_t)luaL_checkinteger(L, 1);
+
+    const bool success = stm32_flash_ispageerased(page);
+
+    lua_pushboolean(L, success);
+
+    return 1;
+}
+
+int efa_program(lua_State *L) {
+    binding_argcheck(L, 2);
+
+    const uint32_t addr = (uint32_t)luaL_checkinteger(L, 1);
+
+    size_t len;
+    const char* data = luaL_checklstring(L, 2, &len);
+
+    bool success = stm32_flash_write(addr, data, len);
+
+    lua_pushboolean(L, success);
+
+    return 1;
+}
+
+#endif  // AP_SCRIPTING_ENABLED
---
+++ libraries/AP_Scripting/evil_flash_access.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "lua/src/lua.hpp"
+
+int efa_read32(lua_State *L);
+int efa_write32(lua_State *L);
+int efa_erase(lua_State *L);
+int efa_erased(lua_State *L);
+int efa_program(lua_State *L);
